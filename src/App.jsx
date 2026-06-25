c6f777b18acdc110e45ae354b75c34d75eb93fd8
Ryan-Aguilar <ryanevo310@gmail.com>
2026-06-25 14:03:14 -0700

    i have finished up to milestone 3 in this version

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,24 @@ import DataCard from './DataCard'
 import characters from './characters.json'
 import { useState } from 'react';
 import { Popover } from "@mui/material";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
 //import characters from './protagonists.json'
 
 
 function App() {
   const [anchorEl, setAnchorEl] = useState(null);
   const [counter, setCounter] = useState(0); // must go inside of your component! State variables cannot be global
-  
+  const [text, setText] = useState('');
+
+  // this is to change the states text. We are saying in this state specifically, we are going to go by the title passed in
+  function handleChange(e) {
+    setText(e.target.value)
+    console.log(e.target.value);
+  }
+
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -27,11 +38,31 @@ function App() {
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
 
+  const targetSearch = characters.filter((character) => {
+    // Return true only if the title includes the search text
+    return character.title.toLowerCase().includes(text.toLowerCase());
+  });
+
   return (
     <>
+      <Box
+        component="form"
+        sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+        noValidate
+        autoComplete="off"
+      ></Box>
+
+      <TextField
+        id="outlined-controlled"
+        label="Controlled - search"
+        value={text}
+        onChange={handleChange}
+      />
+
 
       <Grid spacing={4} container>
-        {characters.map((character) => (
+
+        {targetSearch.map((character) => (
           <Grid key={character.title}>
             <DataCard
               title={character.title}
@@ -54,17 +85,17 @@ function App() {
       >
         Click Me
       </Button>
-      <Popover 
-      id={id}
-      open={open}
-      anchorEl={anchorEl}
-      onClose={handleClose}
-      anchorOrigin={
-        {
-          vertical: "bottom",
-          horizontal: "left"
-        }
-      }>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={
+          {
+            vertical: "bottom",
+            horizontal: "left"
+          }
+        }>
         <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
       </Popover>
 
